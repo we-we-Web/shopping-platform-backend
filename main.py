c18c922297925b93eade09e11c7db8ec2d668ba8
Liujosh1014 <liuchangyan1014@gmail.com>
2024-10-26 19:28:09 +0800

    create a product model and a DAO for product, enable to manage the products inside the database

--- main.py
+++ main.py
@@ -3,21 +3,41 @@ from sqlalchemy import select
 import uvicorn
 from fastapi import FastAPI, Body, Cookie, Header, HTTPException, Depends, status
 from pydantic import BaseModel, Field
-import models
-from database import engine, SessionLocal
+from database import engine, SessionLocal, database
+from databases import Database
 from sqlalchemy.orm import Session
+from models import ProductDAO
+
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
 
-class PostBase(BaseModel):
-    title: str
-    content: str
-    user_id: int
+@app.on_event("startup")
+async def startup():
+    try:
+        await database.connect()
+        print("Database connected successfully.")
+    except Exception as e:
+        print(f"Database connection error: {e}")
 
-class StudentBase(BaseModel):
-    username: str
-    password: str
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+    print("Database disconnected successfully.")
+
+
+class Product(BaseModel):
+    id: int
+    name: str
+    price: int
+    color: str
+    size: str
+    remain_amount: int
+
+class UpdateProduct(BaseModel):
+    price: Optional[int] = None
+    color: Optional[str] = None
+    size: Optional[str] = None
+    remain_amount: Optional[int] = None
 
 def get_db():
     db = SessionLocal()
@@ -28,14 +48,27 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.get('/student', response_model=List[StudentBase])
-async def get_student(db: db_dependency):
-    query = select(models.Student)
-    return db.execute(query).scalars().all()
+#新增產品
+@app.post("/products", response_model=int)
+async def create_product(product: Product):
+    product_data = product.model_dump()
+    product_id = await ProductDAO.create_product(product_data)
+    return product_id
 
-@app.post('/users', status_code=status.HTTP_201_CREATED)
-async def create_user(user: StudentBase, db: db_dependency):
-    db_user = models.Student(**user.dict())
-    db.add(db_user)
-    db.commit()
+# 刪除產品
+@app.delete("/products/{product_id}", response_model=bool)
+async def delete_product(product_id: int):
+    return await ProductDAO.delete_product(product_id)
+
+# 更新產品
+@app.put("/products/{product_id}", response_model=bool)
+async def update_product(product_id: int, update_data: UpdateProduct):
+    return await ProductDAO.update_product(product_id, update_data.model_dump(exclude_unset=True))
+
+# 獲取產品
+@app.get("/products/{product_id}", response_model=Product)
+async def get_product_by_id(product_id: int):
+    return await ProductDAO.get_product_by_id(product_id)
+
+
 
