1e87fb1d5e7021f1b6213ba112bad95cef374659
01257158 <chenhongyu056@gmail.com>
2024-11-10 16:50:59 +0800

    add new props

--- domain/product.py
+++ domain/product.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -13,3 +13,6 @@ class Product(Base):
     color = Column(String(32))
     size = Column(String(32))
     remain_amount = Column(Integer)
+    description = Column(String(256))  
+    categories = Column(String(256))  
+    discount = Column(Float) 
