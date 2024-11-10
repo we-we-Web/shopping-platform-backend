f8b6b2ddcda5f4ea31b33ee507ac6e5aaeab57e5
Ateto1202 <tonysu1204@gmail.com>
2024-11-10 13:10:12 +0800

    Modify the cors auth

--- main.py
+++ main.py
@@ -9,13 +9,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "https://dongyi.hnd1.zeabur.app",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
